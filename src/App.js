2521727969f9267e4d8ba59f88f380134884ec3c
Rahul Nair <rnair1607@gmail.com>
2020-10-26 17:57:42 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm React App</h1>
-        <Person name="Rahul">Hello there!</Person>
-        <Person name="Rohan" />
-      </div>
-    );
+const App = (props) => {
+  const [personsState, setPersons] = useState({
+    persons: [
+      { name: "Rahul", age: 23 },
+      { name: "Rohan", age: 28 },
+    ],
+  });
 
-    // return React.createElement(
-    //   "div",
-    //   { className: "App" },
-    //   React.createElement("h1", null, "Hi")
-    // );
-  }
-}
+  const switchHandler = () => {
+    setPersons({
+      persons: [
+        { name: "Roman", age: 29 },
+        { name: "Rohan", age: 28 },
+      ],
+    });
+  };
+  return (
+    <div className="App">
+      <h1>Hi, I'm React App</h1>
+      <button onClick={switchHandler}>Button</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      />
+    </div>
+  );
+
+  // return React.createElement(
+  //   "div",
+  //   { className: "App" },
+  //   React.createElement("h1", null, "Hi")
+  // );
+};
 
 export default App;
--- src/Person/Person.jsx
+++ src/Person/Person.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const person = ({ name, children }) => {
+const person = ({ name, children, age }) => {
   return (
     <div>
       <p>Hello {name}.</p>
+      {age}
       <p>{children}</p>
     </div>
   );
